b9d37bd9e29c304b202dca47e0baf6278f8a605c
GolamMuid <golammuid@gmail.com>
2024-04-24 17:33:50 +0600

    added logout api

---
+++ src/app/api/logout/route.ts
@@ -0,0 +1,17 @@
+import { connect } from "@/dbConfig/dbConfig";
+
+import { NextResponse } from "next/server";
+
+connect();
+
+export async function GET() {
+	try {
+		const response = NextResponse.json({
+			message: "Logged out successfully",
+			success: true,
+		});
+		response.cookies.set("token", "", { httpOnly: true });
+	} catch (error: any) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
+}
