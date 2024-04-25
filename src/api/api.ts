5cfc438a0319988295b0d08ae47117d4ee340d46
GolamMuid <golammuid@gmail.com>
2024-04-25 15:15:21 +0600

    fixed api error

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,21 +2,21 @@ import axios from "axios";
 
 //? Get ALL Post
 
-export const getALLPost = async (id: string) => {
-  try {
-    const response = await axios.get(`/api/post`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+export const getALLPost = async () => {
+	try {
+		const response = await axios.get(`/api/post`);
+		return response.data;
+	} catch (error) {
+		console.log(error);
+	}
 };
 //? Get logout
 
-export const getLogout = async (id: string) => {
-  try {
-    const response = await axios.get(`/api/logout`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+export const getLogout = async () => {
+	try {
+		const response = await axios.get(`/api/logout`);
+		return response.data;
+	} catch (error) {
+		console.log(error);
+	}
 };
