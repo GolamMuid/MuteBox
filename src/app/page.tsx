4a0ba63d1f794fbdd11c99756a12c2d71805c115
GolamMuid <golammuid@gmail.com>
2024-04-25 17:02:15 +0600

    added footer logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,55 +6,55 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Page = () => {
-  const currentYear = new Date().getFullYear();
+	const currentYear = new Date().getFullYear();
 
-  return (
-    <section className="bg-white relative ">
-      <div className="h-screen lg:overflow-hidden ">
-        <div className="h-full w-full  p-[1vh]">
-          <div className="lg:h-[90vh] max-lg:mb-24">
-            <div className="flex max-lg:flex-col h-full w-full">
-              <div className="w-6/12 max-lg:w-full flex justify-center items-center ">
-                <SparklesPreview />
-              </div>
-              <div className="w-6/12 max-lg:w-full  p-[1vh] lg:px-12 flex items-center justify-center ">
-                <MessageForm />
-              </div>
-            </div>
-          </div>
+	return (
+		<section className="bg-white relative ">
+			<div className="h-screen lg:overflow-hidden ">
+				<div className="h-full w-full  p-[1vh]">
+					<div className="lg:h-[90vh] max-lg:mb-24">
+						<div className="flex max-lg:flex-col h-full w-full">
+							<div className="w-6/12 max-lg:w-full flex justify-center items-center ">
+								<SparklesPreview />
+							</div>
+							<div className="w-6/12 max-lg:w-full  p-[1vh] lg:px-12 flex items-center justify-center ">
+								<MessageForm />
+							</div>
+						</div>
+					</div>
 
-          <div className="lg:h-[10vh]   flex justify-center items-center gap-4">
-            <Image
-              src="/next.svg"
-              width={20}
-              height={80}
-              className="max-h-[40px] w-auto"
-              alt="news"
-            />
-            <div className="text-start">
-              © {currentYear} &nbsp;
-              <Link
-                href="https://www.creativematter.agency/"
-                target="_blank"
-                className="hover:underline cursor-pointer"
-              >
-                Creative Matter
-              </Link>
-              . All rights reserved. <br />
-              Trade marks are owned by or licensed to the &nbsp;
-              <Link
-                href="https://www.atilimited.net/"
-                target="_blank"
-                className="hover:underline cursor-pointer"
-              >
-                ATI Limited.
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+					<div className="lg:h-[10vh]   flex justify-center items-center gap-4">
+						<Image
+							src="/ATI Limited 1.png"
+							width={80}
+							height={20}
+							className="max-h-[80px] w-auto"
+							alt="news"
+						/>
+						<div className="text-start">
+							© {currentYear} &nbsp;
+							<Link
+								href="https://www.creativematter.agency/"
+								target="_blank"
+								className="hover:underline cursor-pointer"
+							>
+								Creative Matter
+							</Link>
+							. All rights reserved. <br />
+							Trade marks are owned by or licensed to the &nbsp;
+							<Link
+								href="https://www.atilimited.net/"
+								target="_blank"
+								className="hover:underline cursor-pointer"
+							>
+								ATI Limited.
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Page;
