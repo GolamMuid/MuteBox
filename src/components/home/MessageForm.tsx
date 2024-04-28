63493ebae8a87366965b4b8425e07ff32c645f04
GolamMuid <golammuid@gmail.com>
2024-04-28 13:37:14 +0600

    updated metadata

--- src/components/home/MessageForm.tsx
+++ src/components/home/MessageForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import { Input } from "../ui/input";
 import { cn } from "@/lib/utils";
 import { Label } from "../ui/label";
--- src/components/home/SparklesPreviewLogin.tsx
+++ src/components/home/SparklesPreviewLogin.tsx
@@ -1,36 +1,36 @@
 "use client";
-import React from "react";
+
 import { SparklesCore } from "../ui/sparkles";
 
 export function SparklesPreviewLogin() {
-  return (
-    <div className="h-[60vh] w-full bg-white flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className=" text-[10vh] leading-[14vh] font-bold text-center text-black relative z-20">
-        Mute Box
-      </h1>
-      <div className="w-full h-[20vh] relative">
-        {/* Gradients */}
+	return (
+		<div className="h-[60vh] w-full bg-white flex flex-col items-center justify-center overflow-hidden rounded-md">
+			<h1 className="text-[10vh] leading-[14vh] font-bold text-center text-black relative z-20">
+				Mute Box
+			</h1>
+			<div className="w-full h-[20vh] relative">
+				{/* Gradients */}
 
-        {/* <div className="absolute inset-x-[.2vh] top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+				{/* <div className="absolute inset-x-[.2vh] top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
         <div className="absolute inset-x-[.2vh] top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
         <div className="absolute inset-x-[.2vh] top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
         <div className="absolute inset-x-[.2vh] top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" /> */}
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+				<div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+				<div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+				<div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+				<div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
-        <SparklesCore
-          background="transparent"
-          minSize={0.4}
-          maxSize={1.5}
-          particleDensity={1200}
-          className="w-full h-full"
-          particleColor="#000000"
-        />
+				<SparklesCore
+					background="transparent"
+					minSize={0.4}
+					maxSize={1.5}
+					particleDensity={1200}
+					className="w-full h-full"
+					particleColor="#000000"
+				/>
 
-        <div className="absolute inset-0 w-full h-full bg-white [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-      </div>
-    </div>
-  );
+				<div className="absolute inset-0 w-full h-full bg-white [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+			</div>
+		</div>
+	);
 }
