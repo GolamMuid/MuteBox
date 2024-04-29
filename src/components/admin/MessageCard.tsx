6df69f0098bab3a4dbb73286723aa535878372ba
GolamMuid <golammuid@gmail.com>
2024-04-29 18:41:19 +0600

    added error fix

--- src/components/admin/MessageCard.tsx
+++ src/components/admin/MessageCard.tsx
@@ -3,13 +3,21 @@
 import { getALLPost } from "@/api/api";
 import { HoverEffect } from "../ui/card-hover-effect";
 import { useQuery } from "@tanstack/react-query";
-import { RingLoader, SyncLoader } from "react-spinners";
+import { RingLoader } from "react-spinners";
 
 export function MessageCard() {
-	const { isLoading, error, data } = useQuery({
+	const { isLoading, isError, data } = useQuery({
 		queryKey: ["getALLPost"],
 		queryFn: getALLPost,
 	});
+
+	if (isError) {
+		return (
+			<div className="text-center text-xl font-bold">
+				Something went wrong, Please reload
+			</div>
+		);
+	}
 
 	return (
 		<div className="container mx-auto px-8 mt-16">
