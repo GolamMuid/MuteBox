a372b7c4271ca7c17ad1be495333824f55d274c4
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2024-04-29 15:43:21 +0600

    api file

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,25 +4,25 @@ import Cookie from "js-cookie";
 //? Get ALL Post
 
 export const getALLPost = async () => {
-	try {
-		const response = await axios.get(`/api/post`);
-		return response.data;
-	} catch (error: any) {
-		// console.log(error.response.data.error);
-		if (error?.response?.data?.error === "invalid signature") {
-			console.log("111");
-			Cookie.remove("token");
-		}
-	}
+  try {
+    const response = await axios.get(`/api/post`);
+    return response.data;
+  } catch (error: any) {
+    // console.log(error.response.data.error);
+    if (error?.response?.data?.error === "invalid signature") {
+      console.log("111");
+      Cookie.remove("token");
+    }
+  }
 };
 
 //? Get logout
 
 export const getLogout = async () => {
-	try {
-		const response = await axios.get(`/api/logout`);
-		return response.data;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await axios.get(`/api/logout`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import Link from "next/link";
+
 export const Menu = ({ children }: { children: React.ReactNode }) => {
   return (
     <nav className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-between space-x-4 px-8 py-4 items-center max-md:flex-col">
       <div className="text-3xl font-bold text-sky-800 max-md:mb-4">
-        Mute Box
+        <Link href="/admin">Mute Box</Link>
       </div>
       {children}
     </nav>
