6e16dbfa5abeb59e874d4171a4c2843f9aa5e36e
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2024-04-29 12:12:43 +0600

    logout solve

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -11,7 +11,7 @@ function Navbar({ className }: { className?: string }) {
   const router = useRouter();
   const logoutRequest = async () => {
     try {
-      const response = await fetch("http://localhost:3000/api/logout", {
+      const response = await fetch("/api/logout", {
         method: "GET",
       })
         .then((res) => {
